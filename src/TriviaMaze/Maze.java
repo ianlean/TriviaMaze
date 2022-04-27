a7aa7e0ae18233fcf713785538925a0309a55386
Ian <ianlean717@gmail.com>
2022-04-27 13:46:56 -0700

    changing some structure

--- src/TriviaMaze/Maze.java
+++
@@ -1,8 +0,0 @@
-package TriviaMaze;
-
-
-public class Maze
-{
-    public static final int SIZE = 10;
-    private Cell[][] maze;
-}
---
+++ src/TriviaMaze/TriviaMaze.java
@@ -0,0 +1,4 @@
+package TriviaMaze;
+
+public class TriviaMaze {
+}

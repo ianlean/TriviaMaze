2bb0517c96de6504e7ba5cdfa8a3576df5870902
taoqinyu <taoqinyu@uw.edu>
2022-05-09 12:03:50 -0700

    add a return statement in generatequestion class

--- src/TriviaMaze/GenerateQuestion.java
+++ src/TriviaMaze/GenerateQuestion.java
@@ -1,14 +1,12 @@
 package TriviaMaze;
 
 import TriviaMaze.Question.Question;
-import TriviaMaze.Question.TrueFalseType;
 
 import java.util.Random;
 
 public class GenerateQuestion
 {
-    private static Database database = new Database();
-    private static Random myRandom;
+    private Random myRandom;
 
     public GenerateQuestion()
     {
@@ -16,21 +14,6 @@ public class GenerateQuestion
     }
     public Question generateRandomQuestion()
     {
-        return createQuestion("tf");
-//        return switch (myRandom.nextInt(2)) {
-//            case 0 -> createQuestion("tf");
-//            case 1 -> createQuestion("multi");
-//            default -> null;
-//        };
-    }
 
-    public Question createQuestion(final String theType) {
-        Question question = null;
-        switch (theType)
-        {
-            case "tf":
-                question = database.getTrueFalseQuestion();
-        }
-        return question;
     }
 }

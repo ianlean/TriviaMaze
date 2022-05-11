fe06f126a0d6c7076b032a903247a9dc346c6466
taoqinyu <taoqinyu@uw.edu>
2022-05-11 14:58:56 -0700

    update SQL and question class

--- src/TriviaMaze/GenerateQuestion.java
+++ src/TriviaMaze/GenerateQuestion.java
@@ -1,11 +1,14 @@
 package TriviaMaze;
 
 import TriviaMaze.Question.Question;
+import TriviaMaze.Question.QuestionType;
+import TriviaMaze.Question.TrueFalseType;
 
 import java.util.Random;
 
 public class GenerateQuestion
 {
+    private static Database database = new Database();
     private Random myRandom;
 
     public GenerateQuestion()
@@ -14,6 +17,22 @@ public class GenerateQuestion
     }
     public Question generateRandomQuestion()
     {
+        return switch (myRandom.nextInt(3)) {
+            case 0 -> createQuestion("tf");
+            case 1 -> createQuestion("single");
+            case 2 -> createQuestion("short");
+            default -> null;
+        };
+    }
 
+    public Question createQuestion(final String theType) {
+        QuestionType type;
+        Question question;
+        switch (theType)
+        {
+            case "tf":
+                type = new TrueFalseType();
+        }
+        return null;
     }
 }

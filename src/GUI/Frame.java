da8bafdf45ecb20f8cc6c9570443d95386da1838
taoqinyu <taoqinyu@uw.edu>
2022-05-25 14:47:07 -0700

    update save and load in Frame

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -5,6 +5,7 @@ import TriviaMaze.Controller;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.Objects;
@@ -73,6 +74,78 @@ public class Frame extends JFrame
         myTextBoxes = new TextPanel();
         this.add(myTextBoxes);
         myTextBoxes.setBounds(500, 500, 800, 400);
+
+        //test for save button
+        //
+        //
+        JButton mySave=new JButton("Save Game");
+        mySave.setMnemonic(KeyEvent.VK_S);
+        mySave.setEnabled(true);
+        mySave.addActionListener(event ->
+        {
+            FileDialog fd = new FileDialog(new JFrame(), "Save Game", FileDialog.SAVE);
+            fd.setVisible(true);
+            if (fd.getFile() == null) return;
+            String fileName = fd.getFile();
+            try
+            {
+                File f = new File(fd.getDirectory(), fileName);
+                f.setWritable(true);
+                FileOutputStream file = new FileOutputStream(f);
+                ObjectOutputStream out =new ObjectOutputStream(file);
+
+                out.writeObject(this);
+
+
+                out.close();
+                file.close();
+            }
+            catch (IOException theE)
+            {
+                theE.printStackTrace();
+            }
+        });
+        buttonPanel.add(mySave);
+
+        //test for load button
+        //
+        //
+        JButton myLoad=new JButton("Load Game");
+        myLoad.setMnemonic(KeyEvent.VK_L);
+        myLoad.setEnabled(true);
+        myLoad.addActionListener(e ->
+        {
+            FileDialog fd = new FileDialog(new JFrame(), "Load Game", FileDialog.LOAD);
+            fd.setVisible(true);
+            if (fd.getFile() == null) return;
+            try
+            {
+                File f = new File(fd.getDirectory(), fd.getFile());
+                FileInputStream file=new FileInputStream(f);
+                ObjectInputStream in=new ObjectInputStream(file);
+
+                Frame g= (Frame) in.readObject();
+                this.add(g);
+
+                in.close();
+                file.close();
+
+            } catch (ClassNotFoundException | IOException ex)
+            {
+                ex.printStackTrace();
+            }
+
+        });
+        buttonPanel.add(myLoad);
+
+
+
+
+
+
+
+
+
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.pack();

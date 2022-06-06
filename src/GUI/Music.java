addf5ca6f0c2e34cab60ec6238b9b0ed4446abda
Kevin Yang <91995544+kevinYang0612@users.noreply.github.com>
2022-06-05 21:24:17 -0700

    Documentation for music class

--- src/GUI/Music.java
+++ src/GUI/Music.java
@@ -1,13 +1,30 @@
 package GUI;
-
+/*
+ * Assignment: Course Project "Trivia Maze"
+ *
+ * Instructor: Tom Capaul
+ *
+ * */
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-
+/**
+ * This is a Music class that allows us to get a music and playing in the background
+ *
+ * @author Bohan Yang, Ian Mclean, Qinyu Tao
+ * @version June 5th 2022
+ */
 class Music
 {
+    /** music clip*/
     private static Clip myClip;
+    
+    /**
+     * Constructs the Music class that accepting a file path to initialize myClip
+     *  
+     * @param thePath, the music we are going to use for the background
+     */
     public Music(URL thePath)
     {
         try
@@ -21,7 +38,12 @@ class Music
             e.printStackTrace();
         }
     }
-
+    /**
+     * this method allows us user to change the background music volumn
+     * 
+     * @param theNewVolumn, the volumn that user wants to set up
+     * @throw IllegalAccessException, now allow the volumn is less 0 or greater than 100
+     */
     public static void changeVolume(int theNewVolume) throws IllegalAccessException
     {
         if (theNewVolume < 0 || theNewVolume >100)
@@ -31,6 +53,10 @@ class Music
         FloatControl fC=(FloatControl) myClip.getControl(FloatControl.Type.MASTER_GAIN);
         fC.setValue(20.0f * (float) Math.log10(theNewVolume /100.0));
     }
+    
+    /**
+     * this method allows the background music to repeatedly play after ends
+     */
     public void loop()
     {
         myClip.setFramePosition(0);
